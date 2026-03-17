63080ad1445663073872a7824c264281ec0d6b29
Yamel Calderon <yamel2010@gmail.com>
2026-03-17 12:10:41 -0500

    funciones

--- Clase7/funciones.py
+++ Clase7/funciones.py
@@ -18,3 +18,42 @@ for x in range (1, 11):
         print(f"{x} x {z} = {x*z}")
     print("")
 
+"""
+funciones
+"""
+
+def suma1():
+    a = int(input("digite el primer numero: "))
+    b = int(input("digite el segundo numero: "))
+    resultado = a+b
+    print(f"el resultado de la suma es: {resultado}")
+
+def suma2(a,b):
+    resultado = a + b
+    print(f"el resultado de la suma es: {resultado}")
+
+def suma3():
+    a = int(input("digite el primer numero: "))
+    b = int(input("digite el segundo numero: "))
+    resultado = a+b
+    return resultado
+
+def suma4(a,b):
+    resultado = a + b
+    return resultado
+
+suma1()
+
+n1 = int(input("digite el primer numero: "))
+n2 = int(input("digite el segundo numero: "))
+suma2(n1,n2)
+# suma2(2,5)
+
+print(suma3())
+
+n1 = int(input("digite el primer numero: "))
+n2 = int(input("digite el segundo numero: "))
+print(suma4(n1, n2))
+
+# suma4(2,5)
+
